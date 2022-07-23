2c5671379b7ad0faa293e3e719afecbe7ad6f9a8
wesley <wesleysantos20012001@gmail.com>
2022-07-22 23:53:53 -0300

    Flshcards shuffled and displayed in the page 02

--- src/GenerateQuestions.js
+++ src/GenerateQuestions.js
@@ -2,12 +2,18 @@ import Question from "./Question/Question";
 
 import React from "react";
 
-function GenerateQuestions({deck}){
+function GenerateQuestions({ deck }) {
     const [open, setOpen] = React.useState(true)
     let num = 0
-    return deck.map((flyer, index) => {
-        num ++;
-        return <Question number={num} open= {open} setOpen= {setOpen} Text={flyer.Text} Answer={flyer.Answer} key={index}/>
+
+    let shuffledDeck = deck
+        .map(value => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value)
+
+    return shuffledDeck.map((flyer, index) => {
+        num++;
+        return <Question number={num} open={open} setOpen={setOpen} Text={flyer.Text} Answer={flyer.Answer} key={index} />
     })
 }
 
